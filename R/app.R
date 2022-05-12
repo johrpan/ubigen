6aefa55eb9d39a135fe07f44fcd7489365f8d93b
Elias Projahn <elias@johrpan.de>
2022-05-12 13:37:53 +0200

    Add host parameter

--- R/app.R
+++ R/app.R
@@ -1,8 +1,9 @@
 #' Run the application server.
 #'
+#' @param host The hostname to serve the application on.
 #' @param port The port to serve the application on.
 #'
 #' @export
-run_app <- function(port = 3464) {
-    runApp(shinyApp(ui, server), port = port)
+run_app <- function(host = "127.0.0.1", port = 3464) {
+    runApp(shinyApp(ui, server), host = host, port = port)
 }
